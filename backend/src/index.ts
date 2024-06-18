1fbb8e4ceb4404db3d7d0e3572b2688f2ab78ebb
kedar000 <140879080+kedar000@users.noreply.github.com>
2024-06-18 11:41:31 +0530

    mod'

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,18 +1,38 @@
-
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
 import { Hono } from 'hono'
 
-const app = new Hono()
+const app = new Hono<{
+  Bindings : {
+    DATABASE_URL : string
+  }
+}>()
 
+
+app.post('/api/v1/blog/signup', async (c) => {
+
+const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+}).$extends(withAccelerate())
+
+const body = await c.req.json();
+
+await prisma.user.create({
+  data : {
+    email : body.email,
+    password : body.password
+  },
+})
+
+
+  return c.text('post signup route')
+})
 
 app.get('/api/v1/blog/:id', (c) => {
   const id = c.req.param('id')
   console.log(id);
   
   return c.text('get route ')
-})
-
-app.post('/api/v1/blog/signup', (c) => {
-  return c.text('post signup route')
 })
 
 app.post('/api/v1/blog/signin', (c) => {
